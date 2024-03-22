8c8d5da249bf00d1325db3ced8dc47915c726caf
Tünzalə <tunzale@Tuni-MacBook-Air.local>
2024-03-22 22:07:42 +0400

    addde login page

---
+++ assets/js/navbar.js
@@ -0,0 +1,43 @@
+"use strict"
+
+let lang = document.querySelector(".language");
+let currency = document.querySelector(".currency");
+let set = document.querySelector(".setting");
+let langList = document.querySelector(".language-list");
+let currencyList = document.querySelector(".currency-list");
+let settingList = document.querySelector(".setting-list")
+
+
+lang.addEventListener("click", function (e) {
+    e.preventDefault();
+    langList.classList.toggle("d-none");
+    currencyList.classList.add("d-none");
+    settingList.classList.add("d-none");
+})
+
+currency.addEventListener("click", function (e) {
+    e.preventDefault();
+    currencyList.classList.toggle("d-none");
+    langList.classList.add("d-none");
+    settingList.classList.add("d-none");
+})
+
+set.addEventListener("click", function (e) {
+    e.preventDefault();
+    settingList.classList.toggle("d-none");
+    currencyList.classList.add("d-none");
+    langList.classList.add("d-none");
+})
+
+
+let productList = document.querySelector(".product-list");
+let productPage = document.querySelector(".product-page");
+
+productList.addEventListener("mouseover", function () {
+    productPage.classList.remove("d-none");
+})
+
+productList.addEventListener("mouseout", function () {
+    productPage.classList.add("d-none");
+})
+
